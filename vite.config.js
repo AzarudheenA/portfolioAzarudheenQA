7b3ac10bd867bd9a0a9d1a663bbb0254fa66d81c
AzarudheenA <142679190+AzarudheenA@users.noreply.github.com>
2025-04-20 01:46:05 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/portfolioAzarudheenQA/', // This must match your repo name
   plugins: [react()],
-  base: '/AzarudheenAportfolio/'
-});
+})
